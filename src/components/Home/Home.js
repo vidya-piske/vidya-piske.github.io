9879e8190dc99760d8c41c92829a6ee4189d7a9b
vidya-piske <vidyapiske123@gmail.com>
2024-10-16 12:08:10 +0530

    Name Change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                HELLO There!{" "}
+                Hi There!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> VIDYA PISKE</strong>
+                <strong className="main-name"> VIDYA GURRAM</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
